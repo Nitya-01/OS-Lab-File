4259663b057050ce7f3ba5c7db6e14c03b0c1594
NityaGupta01 <153364406+Nitya-01@users.noreply.github.com>
2024-04-18 21:53:41 +0530

    Create 09_DekkersAlgo.cpp

---
+++ 09_DekkersAlgo.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <thread>
+#include <semaphore.h>
+
+using namespace std;
+
+const int NUM_THREADS = 2;
+
+// Shared variables
+int turn = 0;
+bool flag[NUM_THREADS] = {false};
+
+// Semaphore declarations
+sem_t sem;
+
+// Thread function for each process
+void process(int id) {
+    for (int i = 0; i < 5; ++i) {
+        // Entry section
+        flag[id] = true;
+        while (flag[1 - id]) {
+            if (turn != id) {
+                flag[id] = false;
+                while (turn != id);
+                flag[id] = true;
+            }
+        }
+
+        // Critical section
+        cout << "Process " << id + 1 << " is in critical section" << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+
+        // Exit section
+        turn = 1 - id;
+        flag[id] = false;
+    }
+}
+
+int main() {
+    // Initialize semaphore
+    sem_init(&sem, 0, 1);
+
+    // Create threads
+    thread t1(process, 0);
+    thread t2(process, 1);
+
+    // Join threads
+    t1.join();
+    t2.join();
+
+    // Destroy semaphore
+    sem_destroy(&sem);
+
+    return 0;
+}
